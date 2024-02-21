55276c6130a9b3509aa6e7072b79455faf182926
Batu Yürüdür <batuhanyurudur@gmail.com>
2024-02-21 11:09:58 +0300

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // This code sums up the even and 4-multiplier numbers until an odd number input.
+
+        // Importing scanner class for inputs and determining the variables;
+        Scanner input = new Scanner(System.in);
+        int number, sum = 0;
+
+        do {
+            System.out.println("Please enter a number: ");
+            number = input.nextInt();
+            if (number % 2 != 0){
+                System.out.println("Program is ending...");
+                break;
+            } else if (number % 4 == 0) {
+                sum += number;
+            }
+        }while (number % 2 == 0);
+
+        // Output of given even and 4-multiplier numbers.
+        System.out.println(sum);
+    }
+}
